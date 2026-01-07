172c1067b365cb16c9c12b009bd4df3a13e00b25
jayasurya <vjayasuryaajp@gmail.com>
2026-01-07 15:35:31 +0530

    fivth commit

---
+++ backend/models/outages.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const outageSchema = new mongoose.Schema({
+  target: String,
+  status: String,
+  ping: String,
+  time: String
+});
+
+module.exports = mongoose.model("Outage", outageSchema);
